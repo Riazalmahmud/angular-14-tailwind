3ff3b3101499db8b4789fde27d4e5a47a835b620
Riazalmahmud <riazalmahmud041@gmail.com>
2022-08-30 13:48:47 +0600

    easyesses-client-v2

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ["./src/**/*.html", "./src/**/*.ts"],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
